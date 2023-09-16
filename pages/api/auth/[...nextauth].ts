744ac926bccb672f61cdcf513ceba922301583ce
Bruno Carvalho Feitosa <brunocarvalhofeitosa@outlook.com>
2023-09-16 10:17:47 -0300

    adding console to debug res of register api

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -55,7 +55,7 @@ export const authOptions: AuthOptions = {
   pages: {
     signIn: '/sign-in'
   },
-  debug: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV === 'production',
   adapter: PrismaAdapter(prismadb),
   session: { strategy: 'jwt' },
   jwt: {
