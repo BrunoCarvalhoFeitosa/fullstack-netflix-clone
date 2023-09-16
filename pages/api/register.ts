8aab5af7156eec8a4f88a6e1a96a3f6d45090234
Bruno Carvalho Feitosa <brunocarvalhofeitosa@outlook.com>
2023-09-15 21:26:58 -0300

    fixing image param on register api

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -27,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         name,
         email,
         hashedPassword,
-        image,
+        image: "",
         emailVerified: new Date(),
       }
     })
