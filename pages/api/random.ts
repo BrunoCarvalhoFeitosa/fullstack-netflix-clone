dc3d871d13769b737c11001a765871eb9fd5d4db
Bruno Carvalho Feitosa <brunocarvalhofeitosa@outlook.com>
2023-10-03 19:31:40 -0300

    fix: backing signup validation

--- pages/api/random.ts
+++ pages/api/random.ts
@@ -8,7 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(405).end()
     }
 
-    await serverAuth(req)
+    await serverAuth(req, res)
 
     const billboardsCount = await prismadb.billboard.count()
     const randomIndex = Math.floor(Math.random() * billboardsCount)
