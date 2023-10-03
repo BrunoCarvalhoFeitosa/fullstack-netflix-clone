0f6fd5c628fcf44a3c10cdbf36515b064b736c2e
Bruno Carvalho Feitosa <brunocarvalhofeitosa@outlook.com>
2023-10-03 10:44:56 -0300

    fix: change register url to absolute

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -2,7 +2,7 @@ import useSWR from "swr"
 import fetcher from "@/lib/fetcher"
 
 const useCurrentUser = () => {
-    const { data, error, isLoading, mutate } = useSWR("/api/current", fetcher)
+    const { data, error, isLoading, mutate } = useSWR("https://fullstack-netflix-app.vercel.app/api/current", fetcher)
 
     return {
         data,
